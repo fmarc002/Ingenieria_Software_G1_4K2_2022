ba81fe410ace2db4634bb98e0b8afea9df11d87c
emmagalzio <emmagalzio@gmail.com>
2022-09-10 20:47:22 -0300

    TP6 DeliverEat casi fin...

---
+++ Practico/TP_Evaluables/Trabajo_Practico_N6/Delivery_Loquesea/app/src/main/java/isw/grupo1/view/ConfirmacionActivity.java
@@ -0,0 +1,14 @@
+package isw.grupo1.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmacionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmacion);
+    }
+}
